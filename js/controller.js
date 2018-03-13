f4dfee60b6181b21263f360c72b35b1fd3250135
Sammy <Sammy>
2018-03-14 00:43:16 +0530

    Separate controller file

---
+++ js/controller.js
@@ -0,0 +1,6 @@
+var app=angular.module('myapp',[]);
+app.controller('myctrl',function($scope) {
+    $scope.name = "Sameer Tiwari";
+    $scope.role="Aspiring Software Developer/ Java Developer";
+    $scope.firstName="njkfans";
+});
