95277f7a70c0b5c7172325b19f6effab0a4bcd58
Sammy <Sammy>
2018-03-14 01:05:24 +0530

    Separate controller file

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,14 @@
 var app=angular.module('myapp',[]);
+
+// app.config(function($routeProvider){
+//     $routeProvider
+//     .when('/',{
+//         controller: 'myctrl',
+//         templateUrl: 'templates/home.html'
+//     })
+//     .otherwise({redirectTo: '/'}); 
+// });
+
 app.controller('myctrl',function($scope) {
     $scope.name = "Sameer Tiwari";
     $scope.role="Aspiring Software Developer/ Java Developer";
