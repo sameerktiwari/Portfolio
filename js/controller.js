984d1006eca18cd03bdab7d01326605c81b107f3
Sammy <Sammy>
2018-03-15 23:46:03 +0530

    templates working (added angular-route.js)

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,18 @@
-var app=angular.module('myapp',[]);
+var app=angular.module('myapp',['ngRoute']);
 
-// app.config(function($routeProvider){
-//     $routeProvider
-//     .when('/',{
-//         controller: 'myctrl',
-//         templateUrl: 'templates/home.html'
-//     })
-//     .otherwise({redirectTo: '/'}); 
-// });
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            controller: 'myctrl',
+            templateUrl: 'templates/home.html'
+        })
+        .otherwise({redirectTo: '/'});
+});
 
 app.controller('myctrl',function($scope) {
     $scope.name = "Sameer Tiwari";
     $scope.role="Aspiring Software Developer/ Java Developer";
     $scope.firstName="njkfans";
 });
+
+
